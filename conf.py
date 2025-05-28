40143c52c9dd7573126fa7cc61a70a02340f69f6
leomartin1e <leomartin1@funmail.site>
2025-05-28 17:32:39 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'Trezor Documentation'
+copyright = '2025, Trezor'
+author = 'Trezor Team'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False,
+    'display_version': True,
+    'logo_only': False,
+}
---
+++ index.rst
@@ -0,0 +1,33 @@
+Welcome to Trezor Documentation
+==============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting-started/index
+   user-guide/index
+   security/index
+   api/index
+   troubleshooting/index
+
+Getting Started
+--------------
+
+Welcome to the Trezor documentation. This guide will help you get started with your Trezor device.
+
+Quick Links
+----------
+
+* :doc:`getting-started/index`
+* :doc:`user-guide/index`
+* :doc:`security/index`
+* :doc:`api/index`
+* :doc:`troubleshooting/index`
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
